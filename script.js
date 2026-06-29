0f87287fc590acfb0e2dc122af0b4d7078b15900
Maaadzia <madzika.beza@gmail.com>
2026-06-29 16:55:50 +0200

    Poland option test
    tried to do openable and closeable poland window
    prototype design
    script to fix

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function updateTime() {
     timeText.innerHTML = currentDate;
     }
 setInterval(updateTime, 1000);
+
+
+
+//openable and closeable poland
+var polandScreen = document.querySelector("#poland")
+
+function closeWindow(element) {
+  element.style.display = "none"
+}
+function openWindow(element) {
+  element.style.display = "flex"
+}
+
+var polandClose = document.querySelector("#polandclose")
+
+var polandOpen = document.querySelector("#polandopen")
+
+polandClose.addEventListener("click", function() {
+  closeWindow(poland);
+});
+
+polandOpen.addEventListener("click", function() {
+  openWindow(poland);
+});
