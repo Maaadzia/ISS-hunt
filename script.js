8cb35ebd240e2152bfb2b6f701fd02c14aeebba3
Maaadzia <madzika.beza@gmail.com>
2026-06-30 11:39:12 +0200

    Open selected windows
    Tried to make openable windows by selecting and clicking done but something is not working, I'll get to this later

--- script.js
+++ script.js
@@ -8,23 +8,45 @@ setInterval(updateTime, 1000);
 
 
 //openable and closeable poland
-var polandScreen = document.querySelector("#poland")
+//var polandScreen = document.querySelector("#poland")
 
-function closeWindow(element) {
-  element.style.display = "none"
-}
-function openWindow(element) {
-  element.style.display = "flex"
-}
+//function closeWindow(element) {
+//  element.style.display = "none"
+//}
+//function openWindow(element) {
+//  element.style.display = "flex"
+//}
 
-var polandClose = document.querySelector("#polandclose")
+//var polandClose = document.querySelector("#polandclose")
 
-var polandOpen = document.querySelector("#polandopen")
+//var polandOpen = document.querySelector("#polandopen")
 
-polandClose.addEventListener("click", function() {
-  closeWindow(poland);
-});
+//polandClose.addEventListener("click", function() {
+//  closeWindow(poland);
+//});
 
-polandOpen.addEventListener("click", function() {
-  openWindow(poland);
-});
+//polandOpen.addEventListener("click", function() {
+//  openWindow(poland);
+//});
+
+const countrySelect = document.getElementById("#countrySelect");
+const doneButton = document.getElementById("#doneButton");
+
+doneButton.addEventListener("click", function() {
+    const selectedCountry = countrySelect.value;
+
+
+    document.getElementById("polandwindow").style.display = "none";
+    document.getElementById("ukwindow").style.display = "none";
+    document.getElementById("germanywindow").style.display = "none";
+
+
+if (selectedCountry === "poland") {
+        document.getElementById("polandwindow").style.display = "block";
+    } else if (selectedCountry === "uk") {
+        document.getElementById("ukwindow").style.display = "block";
+    } else if (selectedCountry === "germany") {
+        document.getElementById("germanywindow").style.display = "block";
+    }
+
+  });
